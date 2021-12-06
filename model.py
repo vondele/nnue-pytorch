8e973b3fc2fc4e0a668fe64f6020f3dce36cab01
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-12-06 22:35:18 +0100

    Combine tweaks

--- model.py
+++ model.py
@@ -273,7 +273,7 @@ class NNUE(pl.LightningModule):
 
     pt = p * self.lambda_ + t * (1.0 - self.lambda_)
 
-    loss = torch.pow(torch.abs(pt - q), 2.6).mean()
+    loss = torch.pow(torch.abs(pt - q), 2.4).mean()
 
     self.log(loss_type, loss)
 
