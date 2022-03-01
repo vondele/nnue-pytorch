4711c31bc1771720795fc5d6f565daab70261af0
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-03-01 20:08:22 +0100

    Use 8 buckets, including pawn diff

--- training_data_loader.cpp
+++ training_data_loader.cpp
@@ -434,7 +434,7 @@ private:
         is_white[i] = static_cast<float>(e.pos.sideToMove() == Color::White);
         outcome[i] = (e.result + 1.0f) / 2.0f;
         score[i] = e.score;
-        psqt_indices[i] = ((e.pos.piecesBB().count() - 1) / 8) * 2 + e.pos.piecesBB().count() % 2;
+        psqt_indices[i] = ((e.pos.piecesBB().count() - 1) / 8) * 2 + e.pos.piecesBB(Piece(PieceType::Pawn, Color::White)).count() == e.pos.piecesBB(Piece(PieceType::Pawn, Color::Black)).count();
         layer_stack_indices[i] = psqt_indices[i];
         fill_features(FeatureSet<Ts...>{}, i, e);
     }
